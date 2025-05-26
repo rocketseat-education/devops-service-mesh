d6077c626af02ef9f33802c90c4b0c0f814a6b7d
EgydioBNeto <egydiobolonhezi@gmail.com>
2025-05-26 18:04:31 -0300

    Criando uma nova versão da nossa aplicação

--- app-ts/src/health/health.service.ts
+++ app-ts/src/health/health.service.ts
@@ -3,12 +3,12 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class HealthService {
   checkHealth(): string {
-    console.log('Chequei a saúde da aplicação!');
-    return 'OK';
+    console.log('Chequei a saúde da aplicação! - V2');
+    return 'OK - V2';
   }
   
   checkReady(): string {
-    console.log('Chequei a prontidão da aplicação!');
-    return 'OK';
+    console.log('Chequei a prontidão da aplicação! - V2');
+    return 'OK - V2';
   }
 }
